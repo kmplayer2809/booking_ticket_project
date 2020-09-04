3407e2b6f7a1d70bd8898d64a16c20f84a80145c
Huynh Trong Di <kmplayer2809@gmail.com>
2020-09-04 18:10:51 +0700

    Updates

---
+++ precache-manifest.c3d6b758acbc684cb60c36098d28bcda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00911d0a84e86493bc754bd654c22034",
+    "url": "/booking_ticket_project/index.html"
+  },
+  {
+    "revision": "1e9b9d364435e653a4c0",
+    "url": "/booking_ticket_project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e68b2eede78a9923c9ba",
+    "url": "/booking_ticket_project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e9b9d364435e653a4c0",
+    "url": "/booking_ticket_project/static/js/2.ab8b443a.chunk.js"
+  },
+  {
+    "revision": "a7b079a06d99e0bdddae2cf2b771ca7a",
+    "url": "/booking_ticket_project/static/js/2.ab8b443a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68b2eede78a9923c9ba",
+    "url": "/booking_ticket_project/static/js/main.9a143302.chunk.js"
+  },
+  {
+    "revision": "a7ed8fd3a09086b0022a",
+    "url": "/booking_ticket_project/static/js/runtime-main.bbf41ead.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/booking_ticket_project/static/media/logo.5d5d9eef.svg"
+  }
+]);
